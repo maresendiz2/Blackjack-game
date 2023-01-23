c6b6c4a2f832f212b7210e76d91a121bb6e672e4
Marco Resendiz <113551818+maresendiz2@users.noreply.github.com>
2023-01-23 21:20:55 +0000

    added play again

--- main.py
+++ main.py
@@ -4,5 +4,16 @@ num = range(1,12)
 firstCard = random.choice(num)
 secondCard = random.choice(num)
 
+
 Dealer(firstCard, secondCard, num)
 
+while True:
+  again = input("Play again(yes/no)?").lower()
+  if again == "no":
+    print("Thank you for playing!")
+    break
+  else:
+    Dealer(firstCard, secondCard, num)
+  
+      
+  
